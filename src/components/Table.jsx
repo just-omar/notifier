ae65e7f6253021dfd168ea89448f7c44696df81d
omar-home-dekstop <medzhidov.omar11@gmail.com>
2022-08-18 23:54:33 +0300

    completed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,58 +1,139 @@
 import React, { useMemo } from "react";
-import { useTable } from "react-table";
+import {
+  useTable,
+  usePagination,
+  useSortBy,
+  useGlobalFilter,
+  useFilters,
+} from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
-import { COLUMNS, GROUPED_COLUMNS } from "./columns";
+import { COLUMNS } from "./columns";
 import "./table.css";
+import { GlobalFilter } from "./GlobalFilter";
+import { ColumnFilter } from "./ColumnFilter";
 
 const Table = () => {
   const memoizedColumns = useMemo(() => COLUMNS, []);
   const memoizedData = useMemo(() => MOCK_DATA, []);
+  const defaultColumn = useMemo(() => {
+    return { Filter: ColumnFilter };
+  }, []);
 
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    gotoPage,
+    pageCount,
+    setPageSize,
+    setGlobalFilter,
+    state,
     prepareRow,
-    footerGroups,
-  } = useTable({
-    columns: memoizedColumns,
-    data: memoizedData,
-  });
+  } = useTable(
+    {
+      columns: memoizedColumns,
+      data: memoizedData,
+      defaultColumn,
+      //   initialState: { pageIndex: 2 },
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
 
+  const { pageIndex, pageSize, globalFilter } = state;
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((hGroup) => (
-          <tr {...hGroup.getHeaderGroupProps()}>
-            {hGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((hGroup) => (
+            <tr {...hGroup.getHeaderGroupProps()}>
+              {hGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-      <tfoot>
-        {footerGroups.map((fGroup) => (
-          <tr {...fGroup.getFooterGroupProps()}>
-            {fGroup.headers.map((column) => (
-              <td {...column.getFooterProps}>{column.render("Footer")}</td>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        <span>
+          | Go to page :{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Показать на странице {pageSize}
+            </option>
+          ))}
+        </select>
+
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
+      </div>
+    </>
   );
 };
 
