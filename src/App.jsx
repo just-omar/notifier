f03632eec98bdda1906d3bdda87224b212a71d51
omar-home-dekstop <medzhidov.omar11@gmail.com>
2022-08-18 23:54:02 +0300

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import Bell from "./components/Bell";
+
 import Table from "./components/Table";
-import SortingTable from "./components/SortingTable";
-import FilteringTable from "./components/FilteringTable";
-import PaginationTable from "./components/PaginationTable";
+
 function App() {
   return (
     <div className="App">
       <Bell />
-      {/* <Table /> */}
-      {/* <SortingTable /> */}
-      {/* <FilteringTable /> */}
-      <PaginationTable />
+      <Table />
     </div>
   );
 }
