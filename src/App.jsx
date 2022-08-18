feb73530968312723b78938814a4e1f2e80c0aa7
omar-home-dekstop <medzhidov.omar11@gmail.com>
2022-08-18 18:10:05 +0300

    readme

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,37 @@ import "./App.css";
 import Modal from "./Modal";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux/es/exports";
-import { deleteNotification } from "./notificationsSlice";
+import { deleteNotification, addNotification } from "./notificationsSlice";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   const dispatch = useDispatch();
-  const notifs = useSelector((state) => state.notificationReducer);
+  const notifications = useSelector((state) => state.notificationReducer);
 
-  useEffect(() => {
-    console.log(notifs);
-  }, [notifs]);
+  // for (let i = 0; i < 8; i++) {
+  //   setTimeout(
+  //     () =>
+  //       dispatch(
+  //         addNotification({
+  //           id: i + 2,
+  //           header: "foo" + i,
+  //           text: "bar" + i,
+  //         })
+  //       ),
+  //     900
+  //   );
+  // }
+
+  // useEffect(() => {
+  //   console.log(notifications);
+  //   }
+  // }, [notifications]);
 
   return (
     <div className="App">
       <header>
-        <div className="counter">{notifs.length}</div>
+        <div className="counter">{notifications.length}</div>
         <button
           onClick={() => setIsOpen((prevVal) => !prevVal)}
           className="bell-btn"
@@ -29,7 +44,7 @@ function App() {
 
       <Modal open={isOpen} onClose={() => setIsOpen(false)}>
         <section>
-          {notifs.map((element) => (
+          {notifications.map((element) => (
             <div className="notification" key={element.id}>
               <h1 className="notification-header">{element.header}</h1>
               <p className="notification-text">{element.text}</p>
--- src/notificationsSlice.js
+++ src/notificationsSlice.js
@@ -11,36 +11,36 @@ const initialState = [
     header: "foo2",
     text: "bar2",
   },
-  {
-    id: 3,
-    header: "foo3",
-    text: "bar3",
-  },
-  {
-    id: 4,
-    header: "foo4",
-    text: "bar4",
-  },
-  {
-    id: 5,
-    header: "foo5",
-    text: "bar5",
-  },
-  {
-    id: 6,
-    header: "foo6",
-    text: "bar6",
-  },
-  {
-    id: 7,
-    header: "foo7",
-    text: "bar7",
-  },
-  {
-    id: 8,
-    header: "foo8",
-    text: "bar8",
-  },
+  //   {
+  //     id: 3,
+  //     header: "foo3",
+  //     text: "bar3",
+  //   },
+  //   {
+  //     id: 4,
+  //     header: "foo4",
+  //     text: "bar4",
+  //   },
+  //   {
+  //     id: 5,
+  //     header: "foo5",
+  //     text: "bar5",
+  //   },
+  //   {
+  //     id: 6,
+  //     header: "foo6",
+  //     text: "bar6",
+  //   },
+  //   {
+  //     id: 7,
+  //     header: "foo7",
+  //     text: "bar7",
+  //   },
+  //   {
+  //     id: 8,
+  //     header: "foo8",
+  //     text: "bar8",
+  //   },
 ];
 
 const notificationsSlice = createSlice({
@@ -50,9 +50,18 @@ const notificationsSlice = createSlice({
     deleteNotification: (state, action) => {
       return state.filter((item) => item.id !== action.payload.id);
     },
+    addNotification: (state, action) => {
+      const newNotif = {
+        id: action.payload.id,
+        header: action.payload.header,
+        text: action.payload.text,
+      };
+      state.push(newNotif);
+    },
   },
 });
 
-export const { deleteNotification } = notificationsSlice.actions;
+export const { deleteNotification, addNotification } =
+  notificationsSlice.actions;
 
 export default notificationsSlice.reducer;
